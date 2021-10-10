e8593c1812825919ff9f78d8f42941653523b03a
Cozart <artemvlasov22@gmail.com>
2021-10-10 15:09:48 +0300

    Произвести рефакторинг кода

--- js/main.js
+++ js/main.js
@@ -3,8 +3,13 @@ const chooseNumber = (min, max) => Math.floor(Math.random() * (Math.max(Math.abs
 
 //Функция, проверяющая длину строки относительно максимально возможной
 // const checkLength = (checkedString, MAX_LENGTH) => checkedString.length <= MAX_LENGTH;
-
 // checkLength();
+
+const PHOTO_ARRAY_LENGTH = 25;
+const MIN_COMMENT_LENGTH = 1;
+const MAX_COMMENT_LENGTH = 11;
+const MIN_LIKES_COUNT = 15;
+const MAX_LIKES_COUNT = 200;
 
 const COMMENT_NAME = {
   1 : 'Артем',
@@ -24,50 +29,36 @@ const COMMENT_TEXT = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-//Функция создания массива айдишников по порядку
-const collectId = (quantity) => {
-  const identifiers = [];
-  for (let index=1; index<= quantity; index++) {
-    identifiers.push(index);
-  }
-  return identifiers;
-};
-
 //Функция создания объекта с данными о комментарии
 const createComment = () => {
   const commentPerson = chooseNumber(1, Object.keys(COMMENT_NAME).length);
   const commentInfo = {
-    // eslint-disable-next-line prefer-template
-    avatar: 'img/avatar-' + commentPerson + '.svg',
+    avatar: `img/avatar-${  commentPerson  }.svg`,
     name: COMMENT_NAME[commentPerson],
     message: COMMENT_TEXT[chooseNumber(0,(COMMENT_TEXT.length-1))],
   };
   return commentInfo;
 };
 
+let photoOrder = 1;
 //Функция создания объекта с описанием фотографии
-let photoOrder = 0;
 const createDescription = () => {
   const descriptionInfo = {
-    id: collectId(25)[photoOrder],
-    // eslint-disable-next-line prefer-template
-    url: 'photos/' + collectId(25)[photoOrder] + '.jpg',
-    // eslint-disable-next-line prefer-template
-    description: 'Описание фотографии ' + collectId(25)[photoOrder],
-    likes: chooseNumber(15,200),
-    comments : Array.from({length: chooseNumber(1,11)}, createComment),
+    id: photoOrder,
+    url: `photos/${  photoOrder  }.jpg`,
+    description: `Описание фотографии ${  photoOrder}`,
+    likes: chooseNumber(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
+    comments : Array.from({length: chooseNumber(MIN_COMMENT_LENGTH, MAX_COMMENT_LENGTH)}, createComment),
   };
   let commentOrder = 1;
   descriptionInfo.comments.forEach((value) => {
-    // eslint-disable-next-line prefer-template
-    value.id =parseInt((descriptionInfo.id.toString(10) + '0' + commentOrder.toString(10)),10);
+    value.id = parseInt((`${descriptionInfo.id.toString(10)  }0${  commentOrder.toString(10)}`), 10);
     commentOrder++;
   });
   photoOrder++;
   return descriptionInfo;
 };
 
-const photoDescriptions = Array.from({length:25}, createDescription);
-// eslint-disable-next-line no-console
-console.log(photoDescriptions);
+const photoDescriptions = Array.from({length:PHOTO_ARRAY_LENGTH}, createDescription);
+photoDescriptions();
 
