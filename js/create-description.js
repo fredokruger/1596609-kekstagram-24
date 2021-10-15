3037f61141d0106d2a3016112e61379b06ad8b1f
Cozart <artemvlasov22@gmail.com>
2021-10-16 00:50:11 +0300

    Изменить функцию создания объекта с описанием  фото, используя замыкания

--- js/create-description.js
+++ js/create-description.js
@@ -7,22 +7,24 @@ const MAX_COMMENT_LENGTH = 40;
 const MIN_LIKES_COUNT = 15;
 const MAX_LIKES_COUNT = 200;
 
-let photoOrder = 1;
-const createDescription = () => {
-  const descriptionInfo = {
-    id: photoOrder,
-    url: `photos/${  photoOrder  }.jpg`,
-    description: `Описание фотографии ${  photoOrder}`,
-    likes: chooseNumber(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
-    comments : Array.from({length: chooseNumber(MIN_COMMENT_LENGTH, MAX_COMMENT_LENGTH)}, createComment),
+const generateDescription = () => {
+  let photoOrder = 1;
+  return () => {
+    const descriptionInfo = {
+      id: photoOrder,
+      url: `photos/${  photoOrder  }.jpg`,
+      description: `Описание фотографии ${  photoOrder}`,
+      likes: chooseNumber(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
+      comments : Array.from({length: chooseNumber(MIN_COMMENT_LENGTH, MAX_COMMENT_LENGTH)}, createComment),
+    };
+    descriptionInfo.comments.forEach((value,index) => {
+      value.id = parseInt((`${descriptionInfo.id.toString(10)  }0${  (index + 1).toString(10)}`), 10);
+    });
+    photoOrder++;
+    return descriptionInfo;
   };
-  let commentOrder = 1;
-  descriptionInfo.comments.forEach((value) => {
-    value.id = parseInt((`${descriptionInfo.id.toString(10)  }0${  commentOrder.toString(10)}`), 10);
-    commentOrder++;
-  });
-  photoOrder++;
-  return descriptionInfo;
 };
+
+const createDescription = generateDescription();
 
 export {createDescription};
