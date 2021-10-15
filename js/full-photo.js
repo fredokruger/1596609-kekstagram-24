994985ab2cf78c765914f7f3027753f0015a3050
Cozart <artemvlasov22@gmail.com>
2021-10-15 18:00:13 +0300

    Вынести необходимые для работы узлы в константы

--- js/full-photo.js
+++ js/full-photo.js
@@ -1,18 +1,30 @@
+const body = document.body;
+//Элементы попапа с большим фото
 const fullPhotoTemplate = document.querySelector('.big-picture');
+const fullPhotoImg = fullPhotoTemplate.querySelector('.big-picture__img img');
+const fullPhotoLikesCount = fullPhotoTemplate.querySelector('.likes-count');
+const fullPhotoCommentsCount = fullPhotoTemplate.querySelector('.comments-count');
+const fullPhotoCommentsCountBlock = fullPhotoTemplate.querySelector('.social__comment-count');
+const fullPhotoCommentsLoader = fullPhotoTemplate.querySelector('.comments-loader');
+const fullPhotoDescription = fullPhotoTemplate.querySelector('.social__caption');
 const fullPhotoCommentsList = fullPhotoTemplate.querySelector('.social__comments');
 const fullPhotoCommentsItem = fullPhotoCommentsList.querySelector('.social__comment');
+const fullPhotoButtonClose = document.querySelector('.big-picture__cancel');
+const KEY_CODE = 'Escape';
+
 fullPhotoCommentsList.innerHTML = '';
 
 ////Функция открытия большого фото
 const openFullPhoto = (maxPhoto) => {
   fullPhotoTemplate.classList.remove('hidden');
-  fullPhotoTemplate.querySelector('.big-picture__img img').src = maxPhoto.url;
-  fullPhotoTemplate.querySelector('.likes-count').textContent = maxPhoto.likes;
-  fullPhotoTemplate.querySelector('.comments-count').textContent = maxPhoto.comments.length;
-  fullPhotoTemplate.querySelector('.social__caption').textContent = maxPhoto.description;
-  fullPhotoTemplate.querySelector('.social__comment-count').classList.add('hidden');
-  fullPhotoTemplate.querySelector('.comments-loader').classList.add('hidden');
-  document.body.classList.add('modal-open');
+  fullPhotoImg.src = maxPhoto.url;
+  fullPhotoLikesCount.textContent = maxPhoto.likes;
+  fullPhotoCommentsCount.textContent = maxPhoto.comments.length;
+  fullPhotoDescription.textContent = maxPhoto.description;
+  fullPhotoCommentsCountBlock.classList.add('hidden');
+  fullPhotoCommentsLoader.classList.add('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', closeFullPhotoOnKey);
 };
 
 //Функция создания комментария
@@ -23,18 +35,23 @@ const createComment = (comment) => {
   fullPhotoCommentClone.querySelector('.social__text').textContent = comment.message;
   fullPhotoCommentsList.appendChild(fullPhotoCommentClone);
 };
+
 //Функция закрытия большого фото
 const closeFullPhoto = () => {
   fullPhotoTemplate.classList.add('hidden');
-  document.body.classList.remove('modal-open');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', closeFullPhotoOnKey);
 };
+
 //Вызов функции закрытия большого фото при нажатии на крестик
-document.querySelector('.big-picture__cancel').addEventListener('click', closeFullPhoto);
+fullPhotoButtonClose.addEventListener('click', closeFullPhoto);
+
 //Вызов функции закрытия большого фото при нажатии на клавишу
-if (fullPhotoTemplate.classList.contains('hidden')) {
-  document.addEventListener('keydown', (evt) => {
-    (evt.key === 'Escape') ? closeFullPhoto() : undefined;
-  });
+function closeFullPhotoOnKey (evt) {
+  if (evt.key !== KEY_CODE) {
+    return;
+  }
+  closeFullPhoto();
 }
 
 document.querySelector('.overlay').addEventListener('click', (evt) => {
